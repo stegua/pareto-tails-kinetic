7eda6909dd5cfa565c73a8180fd7ff6213e70e9c
Stefano Gualandi <stefano.gualandi@gmail.com>
2017-10-15 21:03:41 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pareto tails in socio-economic phenomena: A kinetic description
+
+This repository contains the data sets and an [R script](https://www.r-project.org/) to reproduce the results presented in the following paper:
+
+**Pareto tails in socio-economic phenomena: A kinetic description**, by Gualandi, S. and Toscani G. 2017.
+
+The paper is currently under revision.
+
+## Data sets
+For all the countries, the data were collected on the [City Population](http://citypopulation.de) web site.
+
+The population of the Italian cities were retrieved from the [ISTAT](http://www.istat.it).
+
+## Comments and Feedback
+For any comments, suggestions, or requests, please drop an email to the authors.
+
+## License
+See the attached MIT license.
---
+++ power_law_script.R
@@ -0,0 +1,48 @@
+# FOR A TUTORIAL SEE SECTION 2 in :
+# https://cran.r-project.org/web/packages/poweRlaw/vignettes/b_powerlaw_examples.pdf
+
+# REQUIRED PACKAGE:
+# https://cran.r-project.org/web/packages/poweRlaw/index.html
+
+# Load library
+library("poweRlaw", lib.loc="~/R/win-library/3.2")
+
+# Make plot for each country
+myplot <- function(country, foo) {
+  mydata = read.table(paste("D:\\Ricerca\\Pareto\\", country, "_flat.txt", sep=""))
+
+  x <- Filter(function(x) x > 10^5, mydata$V1) 
+  x <- sapply(x, function(x) 1+round(x/1000))
+  
+  # estimate Pareto (power law) distribution
+  m_sp = displ$new(x)
+  est_sp = estimate_xmin(m_sp)
+  
+  # set paramaters to distribution object
+  m_sp$setXmin(est_sp)
+  
+  # prepare options for plot
+  par(mar=c(3, 3, 2, 1), mgp=c(2, 0.4, 0), tck=-.01, cex.axis=0.9, las=1)
+  
+  # just plot input data
+  plot(m_sp, pch=21, bg=4, xlim=c(100, 20000), ylim=c(0.001, 1.00),
+       panel.first=grid(col="grey80"),xlab="Population size (thousands)", ylab="", 
+       main = paste(foo[country],", #cities=",length(x), ", p=", round(est_sp$pars,3), sep=""))
+  
+  # interpolated line
+  lines(m_sp, col=2, lwd=3)
+}
+
+foo <- vector(mode="list", length=6)
+names(foo) <- c("usa", "japan", "italia", "polonia", "india", "canada")
+foo[[1]] = "USA"
+foo[[2]] = "Japan"
+foo[[3]] = "Italy"
+foo[[4]] = "Poland"
+foo[[5]] = "India"
+foo[[6]] = "Canada"
+
+par(mfrow = c(3, 2))  # 3 rows and 2 columns
+for (i in c("usa", "japan", "italia", "polonia", "india", "canada")) {
+  myplot(i, foo)
+}
